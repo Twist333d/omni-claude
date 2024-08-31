7d8299ed4deff4e247747b911c828bba9cf41c78
AZ <azuev@outlook.com>
2024-08-31 17:48:31 +0400

    Update parser, chunker, and Validator to handle markdown data
    Refactored the `LlamaIndexLoader` to process markdown data correctly and introduced `CustomMarkdownNodeParser` for handling H1/H2 as nodes. Enhanced `LlamaIndexChunker` to support splitting large chunks and validating completeness. Adjusted the `MainProcessor` to handle a maximum number of pages and validate chunk structures more robustly.

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 # FireCrawl Configuration
 FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY")
-FIRECRAWL_API_URL = "https://api.firecrawl.dev/v0"
+FIRECRAWL_API_URL = "https://api.firecrawl.dev/v1"
 
 # Anthropic Configuration
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
@@ -14,6 +14,7 @@ ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 # File Paths
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+LOG_DIR = os.path.join(BASE_DIR, "logs")
 JOB_FILE_DIR = os.path.join(BASE_DIR, "src", "document_ingestion", "data", "jobs")
 JOB_FILE_PATH = os.path.join(JOB_FILE_DIR, "jobs.json")
 RAW_DATA_DIR = os.path.join(BASE_DIR, "src", "document_ingestion", "data", "raw")
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,20 +1,27 @@
 import logging
+import sys
 import os
+from src.utils.config import LOG_DIR
 
 def setup_logger(name, log_file, level=logging.INFO):
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "logs")
-    os.makedirs(log_dir, exist_ok=True)
-    file_handler = logging.FileHandler(os.path.join(log_dir, log_file))
-    file_handler.setFormatter(formatter)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-
     logger = logging.getLogger(name)
+    if logger.handlers:  # Return existing logger if it's already set up
+        return logger
+
     logger.setLevel(level)
+
+    # File handler
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(os.path.join(LOG_DIR, log_file))
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
+
+    # Console handler
+    console_formatter = logging.Formatter('[%(asctime)s - %(name)s:%(lineno)d - %(levelname)s] %(message)s')
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.DEBUG)
+    logger.addHandler(console_handler)
 
     return logger
