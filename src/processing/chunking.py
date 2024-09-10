3107315444218409e1c4bc50bf1e8e5bfa7483f1
AZ <azuev@outlook.com>
2024-09-10 22:00:54 +0400

    Started chunker re-factoring

--- src/processing/chunking.py
+++ src/processing/chunking.py
@@ -1,46 +1,47 @@
+import uuid
 from pprint import pprint
-
-
-from langchain_text_splitters import (
-    Language,
-    RecursiveCharacterTextSplitter,
-    MarkdownHeaderTextSplitter,)
-
-import os
 import json
+import os
+import mistune
+from typing import List, Dict, Any,
 
-from src.utils.config import NEW_RAW_DATA_DIR
+from src.utils.config import NEW_RAW_DATA_DIR, NEW_PROCESSED_DATA_DIR, LOG_DIR
+from src.utils.logger import setup_logger
+from src.utils.decorators import error_handler
 
+logger = setup_logger('chunker', 'chunking.log')
 
-input_filepath=os.path.join(NEW_RAW_DATA_DIR, 'cra_supabase_docs_2024-09-08 22:21:45.json')
+class MarkdownChunker:
+    def __init__(self, input_filename:str, output_dir: str = NEW_PROCESSED_DATA_DIR):
+        self.output_dir = output_dir
+        self.logger = logger
+        self.input_filename = input_filename
 
-with open(input_filepath, 'r', encoding='utf-8') as f:
-    doc = json.load(f)
+    @error_handler
+    def load_data(self) -> Dict[str, Any]:
+        """Loads markdown from json and prepares for chunking"""
+        input_filepath = os.path.join(NEW_RAW_DATA_DIR, self.input_filename)
 
-headers_to_split_on = [
-    ("#", "Header 1"),
-    ("##", "Header 2"),
-]
+        with open(input_filepath, 'r', encoding='utf-8') as f:
+            doc = json.load(f)
+        self.logger.info(f"{self.input_filename} loaded")
 
-markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on,
-                                               strip_headers=False)
+        return doc
 
-# Let's split further
-chunk_size = 1000
-chunk_overlap = chunk_size * 0.1 # 5%
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=chunk_size, chunk_overlap=chunk_overlap
-)
+    @error_handler
+    def save_chunks(self):
+        """Saves chunks to output dir"""
 
-results = []
-for item in doc['data']:
-    markdown = item['markdown']
-    md_header_splits = markdown_splitter.split_text(markdown)
-    text_splits = text_splitter.split_documents(md_header_splits)
-    results.extend(text_splits)
+    def identify_structure(self):
+        """"""
 
-for result in results:
-    print("New chunk")
-    print(f"Metadata: {result.metadata}")
-    print(f"Text: {result.page_content}")
-    print(f"\n\n--------------------\n\n")
+    def _generate_chunk_id(self) -> uuid.UUID:
+        """Generates chunk's uuidv4"""
+        return uuid.uuid4()
+
+    def _calculate_tokens(self, text: str) -> int:
+        """Calculates the number of tokens in a given text using tiktoken"""
+
+# Test usage
+markdown_chunker = MarkdownChunker(input_filename="cra_supabase_docs_2024-09-08 22:21:45.json")
+markdown_chunker.load_data()
