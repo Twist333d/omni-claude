7a36fa520ee916c2b1d61487e50381a879c7f03d
Alexander Zuev <azuev@outlook.com>
2024-10-17 11:11:37 +0400

    feat(chainlit) added chainlit web ui to the application

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from src.core.component_initializer import ComponentInitializer
+from src.ui.chainlit_ui import run_chainlit
 from src.ui.terminal_ui import run_terminal_ui
 from src.utils.decorators import base_error_handler
 from src.utils.logger import configure_logging
 
 
 @base_error_handler
-def main(debug: bool = False, reset_db: bool = False):
+def main(debug: bool = False, reset_db: bool = False, interface: str = "terminal"):
     # Configure logging before importing other modules
     configure_logging(debug=debug)
 
+    # TODO: allow users to select documents locally or pull from a db
     docs = [
         "docs_anthropic_com_en_20240928_135426-chunked.json",
         "langchain-ai_github_io_langgraph_20240928_210913-chunked.json",
@@ -19,8 +21,12 @@ def main(debug: bool = False, reset_db: bool = False):
     initializer = ComponentInitializer(reset_db=reset_db, load_all_docs=False, files=docs)
     claude_assistant = initializer.init()
 
-    run_terminal_ui(claude_assistant)
+    if interface == "web":
+        run_chainlit(claude_assistant)
+    else:
+        run_terminal_ui(claude_assistant)
 
 
 if __name__ == "__main__":
-    main(debug=False, reset_db=False)
+    # TODO: reset DB should be a command a user can use
+    main(debug=False, reset_db=False, interface="web")
